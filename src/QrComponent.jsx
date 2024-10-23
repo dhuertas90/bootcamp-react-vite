76082d9e108e9478c0a988a7e5ea12c610c08706
David <dhuertas90@gmail.com>
2024-10-22 21:42:50 -0300

    Updated componets

--- src/QrComponent.jsx
+++ src/QrComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const QrComponent = () => {
 return (
 <div className="image-container image-background-3">
-    <h2>Código QR</h2>
+    <h2>Comparte QR</h2>
     <img src="/images/qrconsultorio.PNG" alt="Código QR" className="banner-image" />
 </div>
 );
